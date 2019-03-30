851349bf1e1a44020ea15645d83885a63b09295d
zacheryfaria <zacheryfaria@gmail.com>
2019-03-30 15:08:20 -0700

    Fixed last row being set to 0

--- src/read.c
+++ src/read.c
@@ -6,7 +6,7 @@
 /*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 15:06:27 by zfaria            #+#    #+#             */
-/*   Updated: 2019/03/30 14:19:51 by zfaria           ###   ########.fr       */
+/*   Updated: 2019/03/30 15:06:46 by zfaria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int		*convert_to_int(char **str, int len)
 
 	i = 0;
 	tab = ft_memalloc(len * sizeof(int) * 2);
-	while (i < len - 1)
+	while (i < len)
 	{
 		s = ft_strsplit(str[i], ',');
 		tab[i] = ft_atoi(s[0]);
